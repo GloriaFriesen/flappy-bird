637ab5aca91a4c53b737d73d6768e4026b8be7e7
Cameron Jensen and Gloria Friesen <gloria@>
2017-06-13 14:58:53 -0700

    set to portrait layout

--- core/src/com/flappybird/game/states/MenuState.java
+++ core/src/com/flappybird/game/states/MenuState.java
@@ -12,6 +12,7 @@ public class MenuState extends State {
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        cam.setToOrtho(false, flappybird.WIDTH / 2, flappybird.HEIGHT / 2);
         background = new Texture("bg.png");
         playBtn = new Texture("playbtn.png");
     }
@@ -30,9 +31,10 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(background, 0,0, flappybird.WIDTH, flappybird.HEIGHT);
-        sb.draw(playBtn, (flappybird.WIDTH / 2) - (playBtn.getWidth() / 2), flappybird.HEIGHT / 2);
+        sb.draw(background, 0,0);
+        sb.draw(playBtn, cam.position.x - playBtn.getWidth() / 2, cam.position.y);
         sb.end();
     }
 
