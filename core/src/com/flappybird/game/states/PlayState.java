8dc184b5cfb40fb4cbf4a1052219c75cd13a01ad
Cameron Jensen and Gloria Friesen <cameron@>
2017-06-13 14:23:39 -0700

    place ground

--- core/src/com/flappybird/game/states/PlayState.java
+++ core/src/com/flappybird/game/states/PlayState.java
@@ -3,6 +3,7 @@ package com.flappybird.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.flappybird.game.flappybird;
 import com.flappybird.game.sprites.Bird;
@@ -11,9 +12,12 @@ import com.flappybird.game.sprites.Tube;
 public class PlayState extends State {
     private static final int TUBE_SPACING = 125;
     private static final int TUBE_COUNT = 4;
+    private static final int GROUND_Y_OFFSET = -50;
 
     private Bird bird;
     private Texture bg;
+    private Texture ground;
+    private Vector2 groundPos1, groundPos2;
 
     private Array<Tube> tubes;
 
@@ -22,7 +26,11 @@ public class PlayState extends State {
         bird = new Bird(50, 300);
         bg = new Texture("bg.png");
         cam.setToOrtho(false, flappybird.WIDTH / 2, flappybird.HEIGHT / 2);
+        ground = new Texture("ground.png");
+        groundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, GROUND_Y_OFFSET);
+        groundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + ground.getWidth(), GROUND_Y_OFFSET);
         tubes = new Array<Tube>();
+
 
         for (int i = 1; i <= TUBE_COUNT; i++ ) {
             tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
@@ -38,6 +46,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateGround();
         bird.update(dt);
         cam.position.x = bird.getPosition().x + 80;
 
@@ -50,6 +59,9 @@ public class PlayState extends State {
             if(tube.collides(bird.getBounds()))
                 gsm.set(new PlayState(gsm));
         }
+
+        if (bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET)
+            gsm.set(new PlayState(gsm));
 
         cam.update();
     }
@@ -64,6 +76,9 @@ public class PlayState extends State {
             sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
             sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         }
+        sb.draw(ground, groundPos1.x, groundPos1.y);
+        sb.draw(ground, groundPos2.x, groundPos2.y);
+
         sb.end();
     }
 
@@ -71,8 +86,16 @@ public class PlayState extends State {
     public void dispose() {
         bg.dispose();
         bird.dispose();
+        ground.dispose();
         for(Tube tube : tubes)
             tube.dispose();
         System.out.println("Play State Dispose");
+    }
+
+    private void updateGround() {
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPos1.x + ground.getWidth())
+            groundPos1.add(ground.getWidth() * 2, 0);
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPos2.x + ground.getWidth())
+            groundPos2.add(ground.getWidth() * 2, 0);
     }
 }
