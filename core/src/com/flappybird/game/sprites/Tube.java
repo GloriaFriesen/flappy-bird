509c95ec20cf1f677456c1994f3c80d7c666c7b7
Cameron Jensen and Gloria Friesen <cameron@>
2017-06-13 11:32:47 -0700

    add array of tubes

--- core/src/com/flappybird/game/sprites/Tube.java
+++ core/src/com/flappybird/game/sprites/Tube.java
@@ -5,11 +5,10 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
 
-/**
- * Created by Guest on 6/13/17.
- */
 
 public class Tube {
+    public static final int TUBE_WIDTH = 52;
+
     private static final int FLUCTUATION = 130;
     private static final int TUBE_GAP = 100;
     private static final int LOWEST_OPENING = 120;
@@ -40,5 +39,10 @@ public class Tube {
 
     public Vector2 getPosBotTube() {
         return posBotTube;
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
     }
 }
